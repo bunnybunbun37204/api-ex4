e673c9af5e98e4cf91ee706e9cb18d4b2613a105
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2023-10-27 01:49:46 +0700

    update vercel.json

--- index.js
+++ index.js
@@ -25,11 +25,7 @@ const Audio = mongoose.model('Audio', audioSchema);
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-app.use(cors({
-  origin: 'https://vavsa-test.vercel.app/',
-  methods: 'GET,POST',
-  allowedHeaders: 'Content-Type,Authorization',
-}));
+app.use(cors(corsOptions));
 
 // Serve uploaded audio files
 app.use('/uploads', express.static('uploads'));
