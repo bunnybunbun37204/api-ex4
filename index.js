dcfc34e83a447cc9090740a4217b9296455fb61d
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2023-10-29 21:00:59 +0700

    final api

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
-const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
